73d62b2df3abe14db1875e534ca66424e39c06fc
Saing <saing.sab@gmail.com>
2023-06-06 12:14:16 +0700

    main service

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
 use actix_web::{get, web, App, HttpServer};
+use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
 
 struct AppState {
-    app_name: String,
+    todolist_entries: Mutex<Vec<TodolistEntries>>
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct TodolistEntries {
+    id: i32,
+    date: i64,
+    title: String,
 }
 
 #[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
-    format!("Hello {app_name}!")
+async fn index() -> String {
+    "This is a health check".to_string()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let app_data = web::Data::new(AppState {
+        todolist_entries: Mutex::new(vec![])
+    });
+
+    HttpServer::new(move || {
         App::new()
-        .app_data(web::Data::new(AppState {
-            app_name: String::from("Actix Web"),
-        }))
-        .service(index)
+            .app_data(app_data.clone())
+            .service(index)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
